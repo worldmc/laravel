d2d8c23701eb985e499166961f45e791393abfe8
Arjo <arjo0481@gmail.com>
2015-07-22 15:59:27 +0200

    testing a sentinel check.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,8 +6,15 @@
 
 @section('content')
 
-
+@if ( Sentinel::check())
+    you are a logged in user<br>
+@else
+    you are a guest<br>
+@endif
     <h5>test, this is appended to the content.</h5>
+
+
+@endsection
 
 
 @show
